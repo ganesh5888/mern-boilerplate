ff17f952a506ed64a468aa87e9633f9578089cc4
ganesh5888 <ganesh.routhu26@gmail.com>
2020-09-03 15:01:33 +0530

    updating few files

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a773cc365f361c47080ec4f350ca45ad.js"
+  "/precache-manifest.f1283519cb628039695384e439cd43cb.js"
 );
 
 self.addEventListener('message', (event) => {
--- routes/new-index.js
+++ routes/new-index.js
@@ -3,13 +3,14 @@ var express = require('express');
 var router = express.Router();
 
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://<username>:<password>@<cluster>-vgz77.azure.mongodb.net/test?retryWrites=true&w=majority";
+//var url = "mongodb+srv://<username>:<password>@<cluster>-vgz77.azure.mongodb.net/test?retryWrites=true&w=majority";
+var url = "mongodb+srv://demo:infoserver123@cluster1.wz96x.mongodb.net/testdb?retryWrites=true&w=majority";
 
 
 router.get('/', (req, res, next) => {
 	MongoClient.connect(url, function(err, db) {
 		if (err) throw err;
-		var dbo = db.db("<database>");
+		var dbo = db.db("testdemo");
 		dbo.collection("collection1").find({}).toArray(function(err, result) {
 			if (err) throw err;
 			console.log('Mongo data coming in hot')
