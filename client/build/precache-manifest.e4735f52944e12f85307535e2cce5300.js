5de75a50f45739d694ee2e1e09ebdc9acd1a7a4a
ganesh5888 <ganesh.routhu26@gmail.com>
2020-09-03 18:32:52 +0530

    debugging2

---
+++ client/build/precache-manifest.e4735f52944e12f85307535e2cce5300.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d974b17cad189075d2789f1453833308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "509c0ddb64376928a303",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "97dd5a1632f55ec88d0d",
+    "url": "/static/js/2.5a80764f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.5a80764f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "509c0ddb64376928a303",
+    "url": "/static/js/main.2f4b8351.chunk.js"
+  },
+  {
+    "revision": "4387b907a4d64303b5a2",
+    "url": "/static/js/runtime-main.fab42d54.js"
+  }
+]);
