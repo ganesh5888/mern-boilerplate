8ada36bf20aedfa25c565bfdfca0d31e9abb7bc5
ganesh5888 <ganesh.routhu26@gmail.com>
2020-09-03 18:21:13 +0530

    Changing message of home page

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ class App extends React.Component {
     console.log("render bestShows: ", this.state.bestShows)
     return (
       <div>
-        azure-mern-demo
+        welcome to azure-mern-demo Application!!!! You are ready to go....
         <ul>
           {
             Object.keys(this.state.bestShows).map((cur, idx) => (
